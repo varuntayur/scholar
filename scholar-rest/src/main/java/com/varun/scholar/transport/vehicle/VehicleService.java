922883f44cb688b3bcca60b1df6b3abd8e4b2a8c
varuntayur <varuntayur@gmail.com>
2012-11-23 17:03:02 +0530

    packaging changes for ejb/rest integration

--- scholar-rest/src/main/java/com/varun/scholar/transport/vehicle/VehicleService.java
+++ scholar-rest/src/main/java/com/varun/scholar/transport/vehicle/VehicleService.java
@@ -1,5 +1,10 @@
 package com.varun.scholar.transport.vehicle;
 
+import java.util.Hashtable;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -8,10 +13,31 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 
+import org.jboss.as.quickstarts.ejb.remote.stateful.RemoteCounter;
+
 import com.google.gson.Gson;
 
 @Path("/")
 public class VehicleService {
+
+	public VehicleService() {
+
+		final Hashtable jndiProperties = new Hashtable();
+		jndiProperties.put(Context.URL_PKG_PREFIXES,
+				"org.jboss.ejb.client.naming");
+		Context context;
+		try {
+			context = new InitialContext(jndiProperties);
+			RemoteCounter rc = (RemoteCounter) context
+					.lookup("ejb:/scholar-ejb/CounterBean!"
+							+ RemoteCounter.class.getName() + "?stateful");
+			rc.increment();
+			System.out.println("Boom" + rc.getCount());
+		} catch (NamingException e1) {
+			e1.printStackTrace();
+		}
+
+	}
 
 	private final Gson gson = new Gson();
 
@@ -21,11 +47,13 @@ public class VehicleService {
 	public String getVehicle(@QueryParam("_dc") long dc,
 			@QueryParam("page") int page, @QueryParam("start") int start,
 			@QueryParam("limit") int limit) {
-		
+
 		System.out.println("_dc=" + dc + " page=" + page + " start=" + start
 				+ " limit=" + limit);
-		
-		String temp = "{\"total\": 100, \"data\": [" + gson.toJson(new VehicleDao()) + "] }";
+
+		String temp = "{\"total\": 100, \"data\": ["
+				+ gson.toJson(new VehicleDao()) + "] }";
+
 		return temp;
 	}
 
